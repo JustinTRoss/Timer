1e6303771b5bdbf1879451ab73f5e7866465281f
Justin Ross <Ross.Justin.T@gmail.com>
2016-05-29 15:53:39 -0700

    Implement timer using react and create basic html shell.

---
+++ entry.jsx
@@ -0,0 +1,36 @@
+var React = require('react'),
+	ReactDOM = require('reactDOM');
+
+var timer = React.createClass({
+	getInitialState: function() {
+		return {elapsedTime: 0};
+	},
+
+	componentDidMount: function() {
+		this.timer = setInterval(this.tick, 50);
+	},
+
+	componentWillUnmount: function() {
+		clearInterval(this.timer);
+	},
+
+	tick: function() {
+		this.setState({elapsedTime: new Date() - this.props.startTime});
+	},
+
+	render: function() {
+
+		var elapsedTime = Math.round(this.state.elapsedTime / 100);
+
+		var seconds = (elapsedTime / 10).toFixed(1);
+		return (
+			<div>Elapsed Time: <b>{seconds} seconds</b></div>;
+		);
+	}
+});
+
+ReactDOM.render(
+	<timer startTime = {Date.now()} />,
+	document.getElementById('main'));
+
+module.exports = timer;
